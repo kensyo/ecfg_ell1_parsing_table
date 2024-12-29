23d9ca40903f3b809e6384b6ae668f70efbbd7f1
kensyo <takitateokowa@gmail.com>
2024-12-29 18:40:48 +0900

    Image タグを使えるようにする

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: "export"
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/ecfg_ell1_parsing_table',
+  assetPrefix: '/ecfg_ell1_parsing_table'
 };
 
 export default nextConfig;
