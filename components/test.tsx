8227f692d32aaacb3015fc8cc4898e9afaa58d53
kensyo <takitateokowa@gmail.com>
2025-01-13 11:14:28 +0900

    サーバーサイドレンダリングの例

--- components/test.tsx
+++ components/test.tsx
@@ -1,10 +1,8 @@
-'use client';
+import { ECFGWrapper } from "wasm_backend";
 
-import { useEffect } from "react";
-
-export default function Test() {
+export default async function Test() {
     const loadExample = async () => {
-        const wasm = await import(`wasm_backend`);
+        // const wasm = await import(`wasm_backend`);
         // wasm.greet();
         const terminals = ["+", "*", "i", "(", ")"];
         const nonTerminals = ["E", "T", "F"];
@@ -16,7 +14,7 @@ export default function Test() {
         const startSymbol = "E";
 
         // コンストラクタ呼び出し時にJsValueがRust側に渡され、RustでVec<String>などに変換
-        const ecfgWrapper = new wasm.ECFGWrapper(terminals, nonTerminals, productions, startSymbol);
+        const ecfgWrapper = new ECFGWrapper(terminals, nonTerminals, productions, startSymbol);
 
         console.log("is_ell1:", ecfgWrapper.is_ell1());
 
@@ -25,13 +23,19 @@ export default function Test() {
         const isNullable = ecfgWrapper.calculate_nullable(symbols);
         console.log("Nullable?", isNullable);
 
+        return isNullable
     };
 
-    useEffect(() => {
-        loadExample();
-    }, []);
+    const hoge = await loadExample();
+
+    let moji = "";
+    if (hoge) {
+        moji = "yes";
+    } else {
+        moji = "no";
+    }
 
     return (
-        <p>fugapiyo</p>
+        <p>{moji}</p>
     );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+import { access, symlink } from "fs/promises";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,8 +10,40 @@ const nextConfig: NextConfig = {
   },
   basePath: '/ecfg_ell1_parsing_table',
   assetPrefix: '/ecfg_ell1_parsing_table',
-  webpack: (config) => {
+  webpack: (config, {isServer}) => {
     config.resolve.extensions = [...config.resolve.extensions, ".wasm"];
+
+    // https://github.com/vercel/next.js/issues/25852
+    config.plugins.push(
+      new (class {
+        apply(compiler: any) {
+          compiler.hooks.afterEmit.tapPromise(
+            'SymlinkWebpackPlugin',
+            async (compiler: any) => {
+              if (isServer) {
+                const from = join(compiler.options.output.path, '../static');
+                const to = join(compiler.options.output.path, 'static');
+
+                try {
+                  await access(from);
+                  console.log(`${from} already exists`);
+                  return;
+                } catch (error: any) {
+                  if (error.code === 'ENOENT') {
+                    // No link exists
+                  } else {
+                    throw error;
+                  }
+                }
+
+                await symlink(to, from, 'junction');
+                console.log(`created symlink ${from} -> ${to}`);
+              }
+            },
+          );
+        }
+      })(),
+    );
 
     config.experiments = {
       ...(config.experiments || {}),
